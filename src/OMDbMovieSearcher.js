20c6c013d00c6cd5f55ef91cce3515581e753dca
Peteksi <matias.cederberg@hotmail.com>
2025-11-06 10:41:00 +0200

    search box initial

--- src/OMDbMovieSearcher.js
+++ src/OMDbMovieSearcher.js
@@ -1,3 +1,35 @@
-<input>
+import React, { useState } from "react";
 
-</input>
+export default function OMDbMovieSearcher() {
+  const [text, setText] = useState("moro");
+
+  return (
+    
+    <div>
+      <p>{text}</p>
+      <form onSubmit={e => {
+        
+        (async () => {
+          e.preventDefault();
+
+          const rawResponse = await fetch(
+          "http://www.omdbapi.com/?t=" + e.target[0].value + "&apikey=980ebb83",
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ a: 1, b: "Textual content" }),
+            },
+          );
+          const content = await rawResponse.json();
+
+          setText(JSON.stringify(content));
+        })();
+      }}>
+        <input />
+      </form>
+    </div>
+  );
+}
